bb9f92712d3136368f1f582e2ab3e159292a7f64
mengxain0913 <mengxian0913@gmail.com>
2024-04-02 20:20:54 +0800

    update

--- sourceCode/高中生解題系統/d182.cpp
+++ sourceCode/高中生解題系統/d182.cpp
@@ -1,50 +1,29 @@
-/*********************************************************
-# File Name: a.cpp
-# Author: Vincent
-# Mail: mengxian0913@gmail.com
-# Created Time: 一  3/25 01:05:51 2024
-*********************************************************/
-
-#pragma GCC optimize("O3")
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define int long long
-#define ff first
-#define ss second
-#define fastIO cin.tie(nullptr)->sync_with_stdio(false);
-#define INF 0x7FFFFFFF
-#define pb push_back
-#define all(aa) aa.begin(),aa.end()
-#define MOD (int)(1e9+7)
-#define MAXN 20000
 
-
-string getStr(int num, int len) {
-  string s = to_string(num);
-  while(s.size() < len) {
-    s = "0" + s;
+string prefix(int n, int len) { // n = 1, len = 2
+  string w=to_string(n);
+  while (w.size()<len) {
+    w="0"+w;
   }
-  return s;
+  return w;
+
 }
 
-
-void solve(int n){
-  int div = pow(10, n / 2);
-  for(int i = 0; i < div; i++) {
-    for(int j = 0; j < div; j++) {
-      if(i * div + j == (i + j) * (i + j)) {
-        cout << getStr(i, n / 2) << getStr(j, n / 2) << "\n";
+void solve(int n) { // n = 4
+  int dig = pow(10, n / 2); // dig = 100
+  for(int i=0;i<dig;i++){
+    for(int j=0;j<dig;j++){
+      if((i+j)*(i+j) == (i*dig)+j) {
+        cout << prefix(i, n/2) << prefix(j, n/2) << "\n";
       }
     }
   }
-  return;
 }
 
-signed main(){
-  // fastIO;
+int main() {
   int n;
-  while(cin >> n) {
+  while (cin >> n) {
     solve(n);
   }
-  return 0;
 }
