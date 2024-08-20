a43fc82265b8d49116e3ddb29967ca674bb88c56
mengxain0913 <mengxian0913@gmail.com>
2024-08-20 20:30:50 +0800

    add: 08-20 code

---
+++ sourceCode/08-20/a.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+using namespace std;
+
+//  pair<T1, T2> 
+//  vector<T>
+
+int main() {
+ int n;
+ cin >> n;
+ int k =0;
+ vector<pair<int, int>> t(n);
+ for(int i=0;i<n;i++){
+  cin >> t[i].first>>t[i].second;
+ }
+
+  vector<int> res;
+
+  for(int i=0;i<n;i++){
+    if(t[i].second>100) {
+     k=k+(t[i].second-100) *5 ;
+     // storage id
+      res.push_back(t[i].first);
+    }
+  }
+
+  sort(res.begin(), res.end());
+
+  for(int i = 0; i < res.size(); i++) {
+    cout << res[i] << " ";
+  }
+
+  cout << "\n"; 
+  cout << k << "\n";
+
+}
+
